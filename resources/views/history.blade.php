2488cdf84d8b91daf7411d274f2f9b9e7080c52c
Daniel Held <danielxheld@gmail.com>
2025-05-08 19:09:46 +0200

    removed rounded-full from image

--- resources/views/history.blade.php
+++ resources/views/history.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.app :title="__('Dashboard')">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <span class="flex items-center space-x-2">
-            <img src="{{ asset('nightmare-darters-logo-min.png') }}" alt="Logo" class="h-[50px] w-[50px] rounded-full">
+            <img src="{{ asset('nightmare-darters-logo-min.png') }}" alt="Logo" class="h-[50px] w-[50px]">
             <span class="text-white font-semibold">Nightmare Darters - Historie</span>
         </span>
         <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <x-layouts.app :title="__('Dashboard')">
     <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
         <span class="flex items-center space-x-2">
-            <img src="{{ asset('nightmare-darters-logo-min.png') }}" alt="Logo" class="h-[50px] w-[50px] rounded-full">
+            <img src="{{ asset('nightmare-darters-logo-min.png') }}" alt="Logo" class="h-[50px] w-[50px]">
             <span class="text-white font-semibold">Nightmare Darters - Aktive Matches</span>
         </span>
         <div class="grid grid-cols-1 auto-rows-min gap-1">
